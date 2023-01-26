679e382bd3ab16e81797295a9b1bcfee29bb578b
Demaria Woods <dwoods447@gmail.com>
2023-01-26 06:55:27 -0600

    adjusted mobile nav

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -7,10 +7,12 @@ const line3 = document.getElementById('line3')
 
 hamburger.addEventListener('click', () => {
     console.log('Clicked Nav')
-    if(navigation.classList.contains('sm:hidden') || navigation.classList.contains('md:hidden')){
-        // show nav 
+    if(navigation.classList.contains('sm:hidden') || navigation.classList.contains('md:hidden') || navigation.classList.contains('hidden')){
+        // show nav
+        console.log(`showing nav`)
         navigation.classList.remove('sm:hidden')
         navigation.classList.remove('md:hidden')
+        navigation.classList.remove('hidden')
         line1.classList.add('absolute', 'rotate-45')
         line2.classList.add('absolute', '-rotate-45')
         line2.classList.remove('mt-1.5')
@@ -18,8 +20,10 @@ hamburger.addEventListener('click', () => {
         line3.style.display = 'none'
     } else {
         //hide nav
+        console.log(`hiding nav`)
         navigation.classList.add('sm:hidden')
         navigation.classList.add('md:hidden')
+        navigation.classList.add('hidden')
         line1.classList.remove('absolute', 'rotate-45')
         line2.classList.remove('absolute', '-rotate-45')
         line2.classList.add('mt-1.5')
